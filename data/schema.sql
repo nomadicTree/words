e302626a04eab55dc2ce69f82536f77e39ef5720
= <=>
2025-11-04 17:51:15 +0000

    db schema

---
+++ data/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Word (
+    id INTEGER PRIMARY KEY,
+    word TEXT NOT NULL UNIQUE,
+    definition TEXT,
+    characteristics TEXT, -- JSON array
+    examples TEXT,        -- JSON array
+    non_examples TEXT     -- JSON array
+);
+
+CREATE TABLE Subject (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE Course (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    subject_id INTEGER NOT NULL REFERENCES Subject(id),
+    UNIQUE(name, subject_id)
+);
+
+CREATE TABLE Topic (
+    id INTEGER PRIMARY KEY,
+    course_id INTEGER NOT NULL REFERENCES Course(id),
+    code TEXT NOT NULL,
+    name TEXT,
+    UNIQUE(course_id, code)
+);
+
+CREATE TABLE WordTopic (
+    word_id INTEGER NOT NULL REFERENCES Word(id) ON DELETE CASCADE,
+    topic_id INTEGER NOT NULL REFERENCES Topic(id) ON DELETE CASCADE,
+    PRIMARY KEY(word_id, topic_id)
+);
+; 
