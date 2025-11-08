12f537053c64cdeb54eb245ee9763068517aba0e
= <=>
2025-11-08 10:24:42 +0000

    store subject and course in query_params as well as session state

--- app_lib/selection_helpers.py
+++ app_lib/selection_helpers.py
@@ -12,15 +12,21 @@ def get_courses(data, subject):
 
 
 def select_subject(data):
-    if "selected_subject" not in st.session_state:
-        st.session_state["selected_subject"] = None
-
     subjects = get_subjects(data)
     if not subjects:
         st.info("No subjects available.")
         st.stop()
 
-    # Ensure previous value is valid
+    # Check query_params for a subject
+    query_subject = st.query_params.get("subject", None)
+    if query_subject and query_subject in subjects:
+        st.session_state["selected_subject"] = query_subject
+
+    # Check session_state for a subject
+    if "selected_subject" not in st.session_state:
+        st.session_state["selected_subject"] = None
+
+    # Ensure session state's value is valid
     if st.session_state["selected_subject"] not in subjects:
         st.session_state["selected_subject"] = subjects[0]
 
@@ -30,19 +36,29 @@ def select_subject(data):
         index=subjects.index(st.session_state["selected_subject"]),
     )
     st.session_state["selected_subject"] = selected
-    return selected
+
+    # Update query_params to reflect the selected subject
+    if query_subject != selected:
+        st.query_params["subject"] = selected
+    return st.session_state["selected_subject"]
 
 
 def select_course(data, subject):
-    if "selected_course" not in st.session_state:
-        st.session_state["selected_course"] = None
-
     courses = get_courses(data, subject)
     if not courses:
         st.info("No courses for this subject.")
         st.stop()
 
-    # Ensure previous value is valid
+    # Check query_params for a course
+    query_course = st.query_params.get("course", None)
+    if query_course and query_course in courses:
+        st.session_state["selected_course"] = query_course
+
+    # Check session_state for a course
+    if "selected_course" not in st.session_state:
+        st.session_state["selected_course"] = None
+
+    # Ensure session state's value is valid
     if st.session_state["selected_course"] not in courses:
         st.session_state["selected_course"] = courses[0]
 
@@ -52,4 +68,8 @@ def select_course(data, subject):
         index=courses.index(st.session_state["selected_course"]),
     )
     st.session_state["selected_course"] = selected
-    return selected
+
+    # Update query_params to reflect the selected course
+    if query_course != selected:
+        st.query_params["course"] = selected
+    return st.session_state["selected_course"]
