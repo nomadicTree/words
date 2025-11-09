3439492d31218044b78622ec1534e832e9f57c84
= <=>
2025-11-09 13:58:18 +0000

    only try to show time taken and number of results if a search has actually happened

--- search_words.py
+++ search_words.py
@@ -39,11 +39,12 @@ def display_search_results(
         results: search results to display
         query: original search query
     """
-    plural = "s" if len(results) != 1 else ""
-    formatted_time = format_time_text(elapsed_time)
-    st.caption(
-        f"Found {len(results)} result{plural} for {query!r} in {formatted_time}."
-    )
+    if elapsed_time is not None:
+        plural = "s" if len(results) != 1 else ""
+        formatted_time = format_time_text(elapsed_time)
+        st.caption(
+            f"Found {len(results)} result{plural} for {query!r} in {formatted_time}."
+        )
     if results:
         expand_results = len(results) == 1  # Expand if only one result
         for word in results:
