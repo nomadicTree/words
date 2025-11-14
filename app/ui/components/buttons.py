8244905f44dc563c9ed74d014f9c31dc220ab41b
= <=>
2025-11-14 18:27:00 +0000

    change text to "view full model"

--- app/ui/components/buttons.py
+++ app/ui/components/buttons.py
@@ -4,7 +4,7 @@ import streamlit as st
 def details_button(url: str) -> None:
     """Create link button to view full details at given url"""
     st.link_button(
-        label="View full details",
+        label="View full model",
         url=url,
         width="content",
     )
