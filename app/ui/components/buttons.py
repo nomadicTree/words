7feba70e75ccae926fb3fd8c0274303cc96c6405
= <=>
2025-11-17 23:55:38 +0000

    add arrow to details button text

--- app/ui/components/buttons.py
+++ app/ui/components/buttons.py
@@ -7,7 +7,7 @@ VIEW_PAGE = "ui/pages/view.py"
 
 def word_details_button(word: Word) -> None:
     """Create link button to view full details of given word"""
-    if st.button(label="View full details", key=f"details_{word.pk}"):
+    if st.button(label="View full details →", key=f"details_{word.pk}"):
         st.session_state["view_word"] = word.slug
         st.session_state["view_subject"] = word.subject.slug
         st.switch_page(VIEW_PAGE)
@@ -15,7 +15,7 @@ def word_details_button(word: Word) -> None:
 
 def wordversion_details_button(wv: WordVersion, key_prefix: str = "") -> None:
     """Create link button to view full details of given wordversion"""
-    if st.button(label="View full details", key=f"details_{key_prefix}_{wv.pk}"):
+    if st.button(label="View full details →", key=f"details_{key_prefix}_{wv.pk}"):
         st.session_state["view_subject"] = wv.subject_slug
         st.session_state["view_word"] = wv.word_slug
         st.session_state["view_levels"] = wv.level_set_slug
@@ -23,7 +23,7 @@ def wordversion_details_button(wv: WordVersion, key_prefix: str = "") -> None:
 
 
 def searchhit_details_button(hit: SearchHit) -> None:
-    if st.button(label="View full details", key=hit.word_id):
+    if st.button(label="View full details →", key=hit.word_id):
         st.session_state["view_subject"] = hit.subject_slug
         st.session_state["view_word"] = hit.word_slug
         st.session_state["view_levels"] = hit.level_set_slug
