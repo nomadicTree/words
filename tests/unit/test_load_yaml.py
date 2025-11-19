0c4bd7e1568269fe629579e4554cb47393337a5c
= <=>
2025-11-19 22:25:25 +0000

    additional tests

--- tests/unit/test_load_yaml.py
+++ tests/unit/test_load_yaml.py
@@ -1,4 +1,5 @@
 import pytest
+from textwrap import dedent
 from frayerstore.importer.exceptions import (
     YamlLoadError,
     InvalidYamlStructure,
@@ -26,6 +27,20 @@ def test_empty_file(tmp_path):
         load_yaml(empty_file)
 
 
+def test_explicit_null(tmp_path):
+    null_file = tmp_path / "null.yaml"
+    null_file.write_text("null")
+    with pytest.raises(YamlLoadError):
+        load_yaml(null_file)
+
+
+def test_empty_newline(tmp_path):
+    empty_newline_file = tmp_path / "newline.yaml"
+    empty_newline_file.write_text("\n")
+    with pytest.raises(YamlLoadError):
+        load_yaml(empty_newline_file)
+
+
 def test_yaml_not_mapping(tmp_path):
     invalid_mapping_file = tmp_path / "invalid_mapping.yaml"
     invalid_mapping_file.write_text("- a\n- b\n - c")
@@ -35,9 +50,40 @@ def test_yaml_not_mapping(tmp_path):
 
 def test_valid_yaml(tmp_path):
     valid_yaml_file = tmp_path / "valid.yaml"
-    valid_yaml_file.write_text("subject:\n  name: Computing\n  slug: computing\n")
+    valid_yaml_file.write_text(
+        dedent("""
+        subject:
+          name: Computing
+          slug: computing
+        """)
+    )
     data = load_yaml(valid_yaml_file)
     assert isinstance(data, dict)
     assert "subject" in data
     assert data["subject"]["name"] == "Computing"
     assert data["subject"]["slug"] == "computing"
+
+
+def test_valid_nested_yaml(tmp_path):
+    valid_nested_file = tmp_path / "valid_nested.yaml"
+    valid_nested_file.write_text(
+        dedent("""
+        subject:
+          name: Computing
+          slug: computing
+        courses:
+          ocr_h446:
+            name: OCR H446
+            codes:
+              - 2.2.1
+        """)
+    )
+    data = load_yaml(valid_nested_file)
+
+    assert isinstance(data, dict)
+    assert "subject" in data
+    assert data["subject"]["name"] == "Computing"
+    assert data["subject"]["slug"] == "computing"
+    assert "ocr_h446" in data["courses"]
+    assert data["courses"]["ocr_h446"]["name"] == "OCR H446"
+    assert "2.2.1" in data["courses"]["ocr_h446"]["codes"]
