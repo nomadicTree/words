96adbc0b11e730b0110127516d543134ac13340a
= <=>
2025-11-22 11:40:10 +0000

    no longer required

--- tests/unit/importer/test_models.py
+++
@@ -1,9 +0,0 @@
-from frayerstore.importer.models import ImportSubject
-
-
-def test_import_item_with_id():
-    item = ImportSubject(id=None, name="Computing", slug="computing")
-    new_item = item.with_id(10)
-    assert new_item.id == 10
-    assert new_item.name == "Computing"
-    assert new_item.slug == "computing"
